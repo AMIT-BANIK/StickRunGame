d2e6e50f6e4be168b1d9be46325827ce96b32736
AMIT-BANIK <amitbanikspsc@gmail.com>
2025-05-18 14:06:13 +0600

    Add collectible

---
+++ src/shapes/Collectible.java
@@ -0,0 +1,50 @@
+package shapes;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Collectible extends Shape implements Selectable {
+
+    private int size = 20;  // size of the icon (width & height)
+    private boolean selected = false;  // whether collected or not
+    private Color color;
+
+    public Collectible(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.color = Color.YELLOW;  // or use random color
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(color);
+        // draw a simple circle or star
+        g.fillOval(x, y, size, size);
+    }
+
+    @Override
+    public double getArea() {
+        return size * size;  // approximate
+    }
+
+    @Override
+    public Shape contains(int px, int py) {
+        if (px >= x && px <= x + size && py >= y && py <= y + size) {
+            return this;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean getSelected() {
+        return selected;
+    }
+
+    @Override
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
